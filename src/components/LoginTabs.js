115075c4027d3ef72050ef68c24fdd7a0bee3d08
dishi890 <dishimalviya1203@gmail.com>
2025-06-06 11:58:24 +0530

    Final cleanup: package updates and LoginTabs fix

--- src/components/LoginTabs.js
+++ src/components/LoginTabs.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import Login from '../screens/Login'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { StatusBar } from 'react-native'
-import { Entypo } from '@expo/vector-icons'
+import React from 'react';
+import Login from '../screens/Login';
+import Signup from '../screens/Signup';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons'
-import { Ionicons } from '@expo/vector-icons'
-import Signup from '../screens/Signup'
-const pr = 'pr'
-const Tab = createBottomTabNavigator()
+
+const Tab = createBottomTabNavigator();
 
 const LoginTabs = () => {
   return (
     <>
       <StatusBar
-        backgroundColor="black" // Set your desired background color
-        barStyle="white" // Set the content (text) color
+        backgroundColor="black"
+        barStyle="light-content"
       />
       <Tab.Navigator
         screenOptions={{
@@ -27,8 +24,7 @@ const LoginTabs = () => {
             paddingBottom: 5
           },
           headerStyle: {
-            backgroundColor: '#009933'
-            // height:0
+            backgroundColor: '#009933',
           },
           headerTitleStyle: {
             fontWeight: 'bold',
@@ -39,34 +35,39 @@ const LoginTabs = () => {
         }}
       >
         <Tab.Screen
-          name={'Login'}
+          name="Login"
+          component={Login}
           options={{
             tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons name="face-man" size={25} color={focused ? '#d0f595' : 'white'} />
+              <MaterialCommunityIcons
+                name="face-man"
+                size={25}
+                color={focused ? '#d0f595' : 'white'}
+              />
             )
           }}
-        >
-          {() => <Login />}
-        </Tab.Screen>
+        />
 
-        {/* <Tab.Screen
-          name={'Signup'}
+        {/* Optional Signup tab (uncomment if needed) */}
+        {/* 
+        <Tab.Screen
+          name="Signup"
+          component={Signup}
           options={{
             tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons name="face-man-shimmer" size={25} color={focused ? '#d0f595' : 'white'} />
+              <MaterialCommunityIcons
+                name="face-man-shimmer"
+                size={25}
+                color={focused ? '#d0f595' : 'white'}
+              />
             ),
             unmountOnBlur: true
           }}
-        >
-          {() => <Signup />}
-        </Tab.Screen> */}
+        />
+        */}
       </Tab.Navigator>
     </>
-  )
-}
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1
-//   }
-// })
-export default LoginTabs
+  );
+};
+
+export default LoginTabs;
